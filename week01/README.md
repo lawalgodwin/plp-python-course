bb739addc8addbc6149316dff2aa2cfb49692e8e
lawalgodwin <godwinl200@gmail.com>
2024-10-29 14:49:45 +0100

    Submit week01 assignment

---
+++ week01/README.md
@@ -0,0 +1,9 @@
+### A program that personalises greetings based on inputed values:
+- name
+- age
+- location
+
+#### To run the program in a linux environment, run the script on the terminal:
+```bash
+    ./user_input.py
+```
---
+++ week01/user_input.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# A script that personalises greetings to the user
+
+# get user's name
+name = input("Enter your name: ")
+
+# get user's age
+age = input("Enter your age: ")
+
+# get user's location
+location = input("Enter your location: ")
+
+response = f"Hello {name}, you are {age} years old and live in {location}"
+
+print(response)
